baf2f1045ec6c3b6cc4ff063c6451b0d87531e41
MonehConteh <Moneh40824@gmail.com>
2022-06-30 13:41:16 -0700

    Hello puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#includes <stdio.h>
-
+#include <stdio.h>
+/
+*main - Entry point 
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multiligual puzzle");
 return(0);
 }
-
