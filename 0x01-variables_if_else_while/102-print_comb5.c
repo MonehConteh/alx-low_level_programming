e1b6df2d510eec4e6c68ea3685e56caa215b1b58
MonehConteh <Moneh40824@gmail.com>
2022-07-03 04:08:20 -0700

    two two-digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 	putchar(' ');
 	putchar((num2 / 10) + '0');
 	putchar((num2 % 10) + '0');
-if (num1 == 98 && num2 == 99)
+	if (num1 == 98 && num2 == 99)
 	continue;
 	putchar(',');
 	putchar(' ');
