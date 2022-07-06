25889bfb2cf39b512948a393fc05edb4a84a5d0f
MonehConteh <Moneh40824@gmail.com>
2022-07-06 14:46:37 -0700

    Printing

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,7 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if (i > 10)
+			else if (i >= 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar((i % 10) + '0');
@@ -25,7 +25,7 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(j);
 			}
-			else if (j > 10)
+			else if (j >= 10)
 			{
 				_putchar(':');
 				_putchar((j / 10) + '0');
