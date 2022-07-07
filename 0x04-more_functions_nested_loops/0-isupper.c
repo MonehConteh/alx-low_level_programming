7dca754a5121a9af1cdce333ece27171bd617fb3
MonehConteh <Moneh40824@gmail.com>
2022-07-07 12:08:52 -0700

    main.h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - check if a letter is upper
+ * _isupper -> this is a function to check upper case alphas
  * @c: an integer paramer
  *
  * Return: something but success
