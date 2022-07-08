99535ff35b1be34267f91f345a87985f072749b6
MonehConteh <Moneh40824@gmail.com>
2022-07-07 17:08:04 -0700

    Ten alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - entry point
  * print_alphabet_x10 -> prints the lowercase alphabets
- *
  */
 void print_alphabet_x10(void)
 {
@@ -11,8 +9,9 @@ char c;
 for (i = 0; i < 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
-
+{
 _putchar(c);
+}
 _putchar('\n');
 }
 }
