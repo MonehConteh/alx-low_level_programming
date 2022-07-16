6b2aa8a7d12ed4a63aa91c915f520beabd0993e8
MonehConteh <Moneh40824@gmail.com>
2022-07-16 00:02:58 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,16 +18,15 @@ void print_buffer(char *b, int size)
 	while (i < size)
 	{
 		if (i % 10 == 0)
-			printf("%08x: ", i);
+			printf("%08x:", i);
 		for (j < i; j < i + 9; j += 2)
 		{
 			if ((j < size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
+				printf("%02x%02x:", b[j], b[j + 1]);
 			else
 			{
 				while (++j <= i + 10)
 					printf(" ");
-				printf(" ");
 			}
 		}
 		for (j = i; j < i + 9 && j < size; j++)
