0fd8bdafbdbc35fcf3d1951be7cc14d7f0b760aa
MonehConteh <Moneh40824@gmail.com>
2022-07-16 00:49:24 -0700

    magic dream

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,6 +4,7 @@
  * rot13 - encodes a string using rot13
  * @s: input string
  * Return: the pointer to dest
+ *
  */
 char *rot13(char *s)
 {
@@ -17,7 +18,7 @@ char *rot13(char *s)
 		{
 			if (*(s + count) == alphabet[i])
 			{
-				*(s + count) == rot13[i];
+				*(s + count) = rot13[i];
 				break;
 			}
 		}
