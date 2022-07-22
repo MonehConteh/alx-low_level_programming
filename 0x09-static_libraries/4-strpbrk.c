8ca33df80740a5b7b9ce82ee41d8fd7e607e2826
MonehConteh <Moneh40824@gmail.com>
2022-07-22 16:12:19 -0700

    sattic library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - locate first occurance
+ * @s: the string pointed to
+ * @accept: the character to found
+ *
+ * Return: to dest
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+		{
+			return (s + i);
+		}
+	}
+	}
+	return (0);
+}
