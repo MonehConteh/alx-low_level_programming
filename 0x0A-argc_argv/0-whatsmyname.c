92d2fd74bb7276be46af39ed0f6e1fe2bf821d12
MonehConteh <Moneh40824@gmail.com>
2022-07-22 16:52:16 -0700

    My name is Thomas Moneh Conteh

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print name
+ * @argc: size
+ * @argv: character
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void) argc;
+	return (0);
+}
