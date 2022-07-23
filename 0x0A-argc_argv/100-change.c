7dc458812cfc6d1090405332f3f4cfdb7c83aaf7
MonehConteh <Moneh40824@gmail.com>
2022-07-22 17:47:21 -0700

    minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - minimum number of coins to make change
+ * @argc: size
+ * @argv: character
+ * Return: always 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int coin[5] = {25, 10, 5, 2, 1};
+	int x, i, change = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	if (x < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		if (x >= coin[i])
+		{
+		x = x - coin[i];
+		change = change + 1;
+		if (x >= coin[i])
+		{
+			i--;
+		}
+		if (x == 0)
+			{
+			break;
+			}
+		}
+		printf("%d\n", change);
+		return (0);
+	}
+}
